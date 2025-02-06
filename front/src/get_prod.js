c3719e3b52aa325e930c2be847801bd2c91c68e8
Santi <santiagofrau3@gmail.com>
2025-02-06 18:48:25 -0300

    first commit

---
+++ front/src/get_prod.js
@@ -0,0 +1,15 @@
+
+
+export function get_prods(cant){
+      
+    return fetch(`https://fakestoreapi.com/products?limit=${cant}`)
+            .then(res=>res.json())
+            .then(json=>json)
+    
+}
+
+export function get_categories(){
+    return fetch('https://fakestoreapi.com/products/categories')
+    .then(res=>res.json())
+    .then(json=>json)
+}
